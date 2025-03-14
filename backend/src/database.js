b563a672e283c973ae826c4752ddf78c8d16fbac
Ruben Mariscal <92536400+rmargil133@users.noreply.github.com>
2025-03-14 18:17:47 +0100

    Update database.js

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose');
 
+// Cadena de conexión a MongoDB Atlas
+const URI = process.env.MONGODB_URI || 'mongodb+srv://rubenmariscalgil:Ruben12112003@cluster0.eabtu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-// Cadena de conexión
-const URI = process.env.MONGODB_URI || 'mongodb://localhost/dbtest';
-// const URI = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost/dbtest';
-
-
-mongoose.connect(URI)
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('La base de datos ha sido conectada: ', URI))
     .catch(error => console.error('Error al conectar con la base de datos:', error));
 
